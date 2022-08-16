1e40a23cb3cc82edbccbc3607313c13230d880cf
Morgancin <morgangarciahdez@hotmail.com>
2022-08-16 14:59:31 -0500

    veamos 6

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,5 @@
 <?php
-$morganpruebasmaster;
+
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
