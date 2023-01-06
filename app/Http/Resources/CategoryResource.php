2636a84ec60819cb59953e6a98ef2c178610c8c2
Morgancin <morgangarciahdez@hotmail.com>
2023-01-05 18:25:01 -0600

    Upgrade

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,6 +18,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'parent_id' => $this->category_id,
             'subcategories' => CategoryResource::collection($this->whenLoaded('subcategories')),
         ];
     }
